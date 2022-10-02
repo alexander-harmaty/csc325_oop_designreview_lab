483e17f7613528476b67577240078a335601de95
alexander-harmaty <92049896+alexander-harmaty@users.noreply.github.com>
2022-10-02 17:58:17 -0400

    Completed Student.java
    ToDo 1: Make this class a child of Human
    ToDo 2: Fix the resulting errors
    ToDo 3: Add a field for GPA and create setter and getter
    ToDo 4: Add comments to your code

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Human.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Human.java
@@ -6,33 +6,33 @@ package com.mycompany.csc325_oop_designreview_lab;
  */
 public abstract class Human {
     private String name;
-	private String address;
-	private short age;
-	
-	// constructor that takes only two paras
-	public Human(String name, short age) {
-		this.name = name;
-		this.age = age;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public abstract String getAddress() ;
-	
-	public abstract void setAddress(String address);
-	
-	
-	public short getAge() {
-		return age;
-	}
-	
-	public void setAge(short age) {
-		this.age = age;
-	}
+    private String address;
+    private short age;
+
+    // constructor that takes only two paras
+    public Human(String name, short age) {
+            this.name = name;
+            this.age = age;
+    }
+
+    public String getName() {
+            return name;
+    }
+
+    public void setName(String name) {
+            this.name = name;
+    }
+
+    public abstract String getAddress() ;
+
+    public abstract void setAddress(String address);
+
+
+    public short getAge() {
+            return age;
+    }
+
+    public void setAge(short age) {
+            this.age = age;
+    }
 }
--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -3,19 +3,34 @@ package com.mycompany.csc325_oop_designreview_lab;
 
 /**
  *
- * @author MoaathAlrajab
+ * @author Alexander Harmaty
  */
-/**
- *
- * @author MoaathAlrajab
- */
-public class Student {
-	// ToDo 1: Make this class a child of Human
-	
-	// ToDo 2: Fix the resulting errors
-	
-	// ToDo 3: Add a field for GPA and create setter and getter
-	
-	// ToDo 4: Add comments to your code
+public class Student extends Human{
+    double GPA;
+
+    public double getGPA() {
+        return GPA;
+    }
+
+    public void setGPA(double GPA) {
+        this.GPA = GPA;
+    }
+    
+    public Student(String eName, short eAge, double eGPA) {
+        super(eName, eAge);
+        this.GPA = eGPA;
+    }
+
+    @Override
+    public String getAddress() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+        // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setAddress(String address) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+        // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 
 }
